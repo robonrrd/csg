20bf08624942889c223629cbc33464f89517390d
Jeffrey Smith <jeffreys@nvidia.com>
2020-04-07 11:36:59 -0700

    Bug fixes and cleanup. Works in limited cases now

--- include/trimesh.h
+++ include/trimesh.h
@@ -1,16 +1,19 @@
 #include <Eigen/Core>
 #include <Eigen/StdVector>
-
 #include <string>
 #include <iostream>
-
 #include "aabb.h"
 
 #pragma once
 
+// Some macros to make the aligned std::vector code cleaner
+#define VECTOR3D Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>
+#define VECTOR2D Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>
+#define VECTOR3I Eigen::Vector3i, Eigen::aligned_allocator<Eigen::Vector3i>
+
+
 namespace CSG
 {
-
 
 //> Triangle holds information for a single triangle, including indices into
 //> its owning TriMesh
@@ -38,44 +41,38 @@ class TriMesh
    // Construct triangle mesh from flat vectors
    TriMesh(const std::vector<double>& vertices, const std::vector<unsigned int>& faces);
 #ifndef SWIG
-   TriMesh(const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>& vertices,
-           const std::vector<Eigen::Vector3i, Eigen::aligned_allocator<Eigen::Vector3i>>& faces);
+   TriMesh(const std::vector<VECTOR3D>& vertices,
+           const std::vector<VECTOR3I>& faces);
 #endif
    // Load OBJ file and return the number of vertices in the new mesh.
    // Returning '0' means the operation failed
    uint32_t loadOBJ(const std::string& filename);
+#ifndef SWIG
    friend std::ostream &operator<<( std::ostream &output, const TriMesh &M );
-
-   const std::vector<Triangle>& faces() const { return m_faces; }
+#endif
 
    // Non-Eigen types for Python binding
    std::vector<double> mesh_vertices() const;
    std::vector<double> mesh_normals() const;// vertex normals
    std::vector<double> mesh_uvs() const;
    std::vector<unsigned int> mesh_faces() const;
+
 #ifndef SWIG
-   const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>& vertices() const
-   { return m_vertices; }
+   // Accessors that we don't wish to expose to Python
+   const std::vector<VECTOR3D>& vertices() const      { return m_vertices; }
+   std::vector<VECTOR3D>& vertices()                  { return m_vertices; }
 
-   const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>& normals() const
-   { return m_normals; }
+   const std::vector<VECTOR3D>& normals() const       { return m_normals; }
+   std::vector<VECTOR3D>& normals()                   { return m_normals; }
 
-   const std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>>& uvs() const
-   { return m_uvs; }
+   const std::vector<VECTOR2D>& uvs() const           { return m_uvs; }
+   std::vector<VECTOR2D>& uvs()                       { return m_uvs; }
+
+   const std::vector<Triangle>& faces() const         { return m_faces; }
+   std::vector<Triangle>& faces()                     { return m_faces; }
+   void setFaces(const std::vector<VECTOR3I>& in);
 
    Eigen::Vector3d faceNormal(uint32_t f) const;
-
-   void setVertices(const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>& in)
-   { m_vertices = in;}
-
-   void setNormals(const std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>>& in)
-   { m_normals = in; }
-
-   void setUVs(const std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>>& in)
-   { m_uvs = in; }
-
-   void setFaces(const std::vector<Triangle>& in) { m_faces = in; }
-   void setFaces(const std::vector<Eigen::Vector3i, Eigen::aligned_allocator<Eigen::Vector3i>>& in);
 #endif
 
    AABBTree createAABBTree() const;
@@ -84,9 +81,9 @@ class TriMesh
    Eigen::Vector3d faceNormalImpl(uint32_t f) const;
 
    // Primal data
-   std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> m_vertices;
-   std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> m_normals;
-   std::vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>> m_uvs;
+   std::vector<VECTOR3D> m_vertices;
+   std::vector<VECTOR3D> m_normals;
+   std::vector<VECTOR2D> m_uvs;
    // Faces index into the vectors above
    std::vector<Triangle> m_faces;
 };
